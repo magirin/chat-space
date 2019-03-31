27cee9a5bb4f3c3247222fb876fd370513744e78
MAGIJIG <magijigu@gmail.com>
2019-03-31 16:36:54 +0900

    permit name email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ def edit
 end
 
 def update
+  if current_user.update(user_params)
+    redirect_to root_path
+  else
+    render :edit
+  end
 end
 
 private
 
   def user_params
-    params.requier(:user).(:name, :email)
+    params.requier(:user).permit(:name, :email)
   end
 
 end
