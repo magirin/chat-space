d836e066f047989e646a1d13a80e8a17f2c2873a
MAGIJIG <magijigu@gmail.com>
2019-04-04 23:04:46 +0900

    define group_controller new create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,17 +1,27 @@
 class GroupsController < ApplicationController
-:new, :create, :edit, :update
 
-def new
-end
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
 
-def create
-end
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+  end
 
-def edit
-end
+  def edit
+  end
 
-def update
-end
+  def update
+  end
 
+  private
+  def group_params
+    params.require(:group).permit(:name,{ :user_ids => []})
+  end
 
 end
