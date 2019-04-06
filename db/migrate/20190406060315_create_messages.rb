3f084769d4ac2aee04e98eab93f855cd1a7b01cc
MAGIJIG <magijigu@gmail.com>
2019-04-06 15:12:05 +0900

    migrate message

--- db/migrate/20190406060315_create_messages.rb
+++ db/migrate/20190406060315_create_messages.rb
@@ -3,6 +3,10 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
 
       t.timestamps
+      t.string :content
+      t.string :image
+      t.references :group, foreign_key: true
+      t.references :user, foregin_key: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190402095547) do
+ActiveRecord::Schema.define(version: 20190406060315) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 20190402095547) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "content"
+    t.string   "image"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -41,4 +52,5 @@ ActiveRecord::Schema.define(version: 20190402095547) do
   end
 
   add_foreign_key "group_users", "groups"
+  add_foreign_key "messages", "groups"
 end
