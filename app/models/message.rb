9364a30a22f55a2f5675c6c6ea12b6aff421efd5
MAGIJIG <magijigu@gmail.com>
2019-04-07 00:17:30 +0900

    fix worng spell of model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,7 @@
 class Message < ApplicationRecord
 
 belongs_to :user
-belongs_to :room
-
+belongs_to :group
 validates :content, presence: true, unless: :image?
 
 mount_uploader :image, ImageUploader
