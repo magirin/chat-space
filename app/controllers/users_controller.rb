dfc774b798d861909c6e894aee417173da423294
MAGIJIG <magijigu@gmail.com>
2019-04-29 20:27:38 +0900

    add index to users name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,27 @@
 class UsersController < ApplicationController
 
-def edit
-end
-
-def update
-  if current_user.update(user_params)
-    redirect_to root_path
-  else
-    render :edit
+  def edit
   end
-end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @user = User.all
+    # if @user
+    #   respond_to do |format|
+    #     format.html 
+    #     format.json
+    #   end
+    #   # flash[:notice] = 'メッセージが送信されました'
+    # end
+  end
+
 
 private
 
