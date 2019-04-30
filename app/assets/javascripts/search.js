0ee7dc5d2ab90cdcc1b77563583953ba660a8d15
MAGIJIG <magijigu@gmail.com>
2019-04-30 16:33:02 +0900

    add html when type something

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,16 @@
-$('').on('keyup', function()
-    var input = 
+$(function(){
+
+    function buildHTML(user){
+        var html = `<div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${user.name}</p>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+                    </div>`
+        return html;
+    };
+
+$('#user-search-field').keyup(function(){
+    var input = $("#user-search-field").val();
+
     $.ajax({
         url: '/users',
         type: "GET",
@@ -9,11 +20,19 @@ $('').on('keyup', function()
         contentType: false
     })
 
-    .done(function(){
-
+    .done(function(data){
+        // var html = buildHTML(data)
+            Array.from(data).forEach(function(idvdata){
+            var html = buildHTML(idvdata)
+            $('#chat-group-users').append(html);
+            console.log(html)
+            console.log(data)
+          });
     })
 
     .fail(function(){
         alert('失敗');
     })
-)
+});
+
+})
