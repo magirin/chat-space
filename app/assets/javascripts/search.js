a2333b2c3170fa0acf2df72407e47392f3ef83f4
MAGIJIG <magijigu@gmail.com>
2019-04-30 18:35:54 +0900

    add incrementalsearch

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    function buildHTML(user){
+    function addUser(user){
         var html = `<div class="chat-group-user clearfix">
                     <p class="chat-group-user__name">${user.name}</p>
                     <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
@@ -8,29 +8,55 @@ $(function(){
         return html;
     };
 
-$('#user-search-field').keyup(function(){
-    var input = $("#user-search-field").val();
+    function deleteUser(name, id){
+        var html = `<div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${name}</p>
+                    <div class="user-search-delete chat-group-user__btn chat-group-user__btn--delete" data-user-id=${id} data-user-name=${name}>削除</div>
+                    </div>`
+        return html;
+    };
 
-    $.ajax({
-        url: '/users',
-        type: "GET",
-        data: {searchWord: input},
-        dataType: 'json',
-    })
 
-    .done(function(data){
-        // var html = buildHTML(data)
-            Array.from(data).forEach(function(idvdata){
-            var html = buildHTML(idvdata)
+
+    $('#user-search-field').keyup(function(){
+        var input = $("#user-search-field").val();
+
+        $.ajax({
+            url: '/users',
+            type: "GET",
+            data: {searchWord: input},
+            dataType: 'json',
+        })
+
+        .done(function(data){
+                Array.from(data).forEach(function(idvdata){
+                var html = addUser(idvdata)
+                $('#user_search_result').append(html);
+            });
+        })
+
+        .fail(function(){
+            alert('失敗');
+        })
+    });
+
+    $(function(){
+        $(document).on("click", ".user-search-add", function(){
+            var name = $(this).attr("data-user-name");
+            var id = $(this).attr("data-user-id");
+            var html = deleteUser(name, id)
+            $(this).parent().remove();
             $('#chat-group-users').append(html);
-            console.log(html)
-            console.log(data)
+        });
+        $(document).on("click", '.user-search-delete', function() {
+            $(this).parent().remove();
           });
     })
 
-    .fail(function(){
-        alert('失敗');
-    })
-});
+    $(function(){
+        $('chat-group-form__action-btn').on('submit', function(){
+            var user_id= $('data-user-id')
 
+        })
+    })
 })
