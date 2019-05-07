885df929ad1070478ebea16b90d73fb55ffe197b
MAGIJIG <magijigu@gmail.com>
2019-05-07 23:31:39 +0900

    delete unnessary git  url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:https://github.com/magirin/chat-space.git"
+set :repo_url, "https://github.com/magirin/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
