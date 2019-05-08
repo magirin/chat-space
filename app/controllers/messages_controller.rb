30d6ea71d02a152de586a849f16d1dc3b787c91b
MAGIJIG <magijigu@gmail.com>
2019-05-08 21:56:10 +0900

    change flash

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,11 +12,11 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     @message.user = current_user
     if @message.save
+      flash[:notice] = 'メッセージが送信されました'
       respond_to do |format|
         format.html {redirect_to group_messages_path(@group)}
         format.json
       end
-      flash[:notice] = 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
