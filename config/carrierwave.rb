1dc718d310148fff871d598f8efc7c78cbcffca3
MAGIJIG <magijigu@gmail.com>
2019-05-11 15:45:42 +0900

    change fog setting

--- config/carrierwave.rb
+++ config/carrierwave.rb
@@ -4,9 +4,9 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage = :fog
-  config.fog_provider = 'fog/aws'
+  config.fog_provider = 'fog/rackspace/storage'
   config.fog_credentials = {
-    provider: 'AWS',
+    provider: 'Rackspace',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1' #例 'ap-northeast-1'
