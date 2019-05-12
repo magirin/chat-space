61080177fdc3eadcd822fbadc4517038c2ba4322
MAGIJIG <magijigu@gmail.com>
2019-05-12 13:46:00 +0900

    change path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,11 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 
-working_directory "#{app_path}/chat-space"
-listen "#{app_path}/chat-space/tmp/sockets/unicorn.sock"
-pid "#{app_path}/chat-space/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/chat-space/log/unicorn.stderr.log"
-stdout_path "#{app_path}/chat-space/log/unicorn.stdout.log"
+working_directory "#{app_path}/current"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
