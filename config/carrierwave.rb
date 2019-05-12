2e9b8bca290f3c36ef2b917280e3b04d356bbe05
MAGIJIG <magijigu@gmail.com>
2019-05-12 17:13:49 +0900

    change fog to aws

--- config/carrierwave.rb
+++ config/carrierwave.rb
@@ -4,9 +4,9 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage = :fog
-  config.fog_provider = 'fog/rackspace/storage'
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider: 'Rackspace',
+    provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
@@ -15,3 +15,4 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'chatspace-pix'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-pix'
 end
+storage :fog
