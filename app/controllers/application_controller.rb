51755482c2292d2577944ae8bccec855418771d7
MAGIJIG <magijigu@gmail.com>
2019-05-19 10:10:20 +0900

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
   protect_from_forgery with: :exception
 
 private
 
 def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+end
+
+def basic_auth
+  authenticate_or_request_with_http_basic do |username, password|
+    username == 'admin' && password == '2222'
+  end
 end
 
 end
